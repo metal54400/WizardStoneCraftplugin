9ae093d4d200e0d9178482f9de9d46dd9e32ceee
metal54400 <audecillier@hotmail.fr>
2025-01-11 20:23:37 +0100

    V14.0.4

---
+++ src/main/java/simpleRep/RepReloadCommand.java
@@ -0,0 +1,47 @@
+package simpleRep;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class RepReloadCommand implements TabExecutor {
+    private String reloadMessages;
+    private final ReputationPlugin plugin;
+
+    public RepReloadCommand(ReputationPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("reputation.reload")) {
+            sender.sendMessage(reloadMessages);
+            String reloadMessages = ChatColor.translateAlternateColorCodes('&',
+                    plugin.getConfig().getString("messages.noperm", "Vous n'avez pas la permission d'utiliser cette commande"));
+            return true;
+        }
+
+        // Recharger la configuration et les messages
+        plugin.reloadConfig();
+        plugin.loadMessages();
+
+        // Lire le message depuis config.yml
+        String reloadMessage = ChatColor.translateAlternateColorCodes('&',
+               plugin.getConfig().getString("messages.reload_success", "&aConfiguration et messages rechargés avec succès!"));
+
+        sender.sendMessage(reloadMessage); // Envoyer le message au joueur
+        return true;
+
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return List.of();
+    }
+}
+
+
